1c948ba01481d7e62c81cab42fdf7c876d993f38
rahmedzaizi <rahmed@zaizi.com>
2022-11-30 10:51:46 +0000

    adding permission boundary to roles

--- lambda.tf
+++ lambda.tf
@@ -46,32 +46,3 @@ resource "aws_lambda_function" "tre_dlq_slack_alerts" {
     "ApplicationType" = "Python"
   }
 }
-
-# TRE-Forward Lambda
-
-# resource "aws_lambda_function" "tre_forward" {
-#   image_uri     = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/tre-forward:${var.common_image_versions.tre_forward}"
-#   package_type  = "Image"
-#   function_name = "${var.env}-${var.prefix}-forward"
-#   role          = aws_iam_role.tre_forward_lambda_role.arn
-#   timeout       = 30
-#   environment {
-#     variables = {
-#       "TRE_OUT_TOPIC_ARN" = aws_sns_topic.tre_out.arn
-#     }
-#   }
-#   tracing_config {
-#     mode = "Active"
-#   }
-
-#   tags = {
-#     "ApplicationType" = "Python"
-#   }
-# }
-
-# resource "aws_lambda_event_source_mapping" "tre_forward_sqs" {
-#   batch_size                         = 1
-#   function_name                      = aws_lambda_function.tre_forward.function_name
-#   event_source_arn                   = aws_sqs_queue.tre_forward.arn
-#   maximum_batching_window_in_seconds = 0
-# }
