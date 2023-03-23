c3c5c27cf70a0a8b70184f818e2f3421626e03e9
Mark Kingsbury <mark@kingsburydata.com>
2023-03-23 15:18:12 +0000

    DTE-649: add filter_policy_scope variable for sns internal

--- sns.tf
+++ sns.tf
@@ -46,11 +46,12 @@ resource "aws_sns_topic_policy" "tre_internal" {
 }
 
 resource "aws_sns_topic_subscription" "tre_internal" {
-  for_each      = { for sub in var.tre_internal_subscriptions : sub.name => sub }
-  topic_arn     = aws_sns_topic.tre_internal.arn
-  protocol      = each.value.protocol
-  endpoint      = each.value.endpoint
-  filter_policy = each.value.filter_policy
+  for_each            = { for sub in var.tre_internal_subscriptions : sub.name => sub }
+  topic_arn           = aws_sns_topic.tre_internal.arn
+  protocol            = each.value.protocol
+  endpoint            = each.value.endpoint
+  filter_policy       = each.value.filter_policy
+  filter_policy_scope = each.value.filter_policy_scope
 }
 
 # TRE Out SNS Topic
