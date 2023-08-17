5d713a36661676278ba459d12b47ff81a2dbfc29
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-08-17 11:48:04 +0100

    feat: Add s3 delete permission on common data bucket

--- iam.tf
+++ iam.tf
@@ -139,6 +139,7 @@ data "aws_iam_policy_document" "common_tre_data_bucket" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:ListBucket",
+      "s3:DeleteObject",
     ]
 
     principals {
