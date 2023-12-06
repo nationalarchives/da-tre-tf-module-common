9314d4185bdb2a44168701681bf3022101dd099f
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-12-06 17:58:32 +0000

    Update to once a day at 7

--- lambda.tf
+++ lambda.tf
@@ -110,9 +110,9 @@ resource "aws_lambda_function" "process_monitoring_queue" {
   }
 }
 
-resource "aws_cloudwatch_event_rule" "every_fifteen_minutes" {
-  name                = "${var.env}-${var.prefix}-every-fifteen-minutes"
-  schedule_expression = "cron(*/15 * * * ? *)"
+resource "aws_cloudwatch_event_rule" "every_day_at_7_am_gmt" {
+  name                = "${var.env}-${var.prefix}-every-day-at-7-am-gmt"
+  schedule_expression = "cron(0 7 * * ? *)"
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch" {
@@ -120,10 +120,10 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.process_monitoring_queue.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.every_fifteen_minutes.arn
+  source_arn    = aws_cloudwatch_event_rule.every_day_at_7_am_gmt.arn
 }
 
 resource "aws_cloudwatch_event_target" "invoke_lambda" {
-  rule = aws_cloudwatch_event_rule.every_fifteen_minutes.name
+  rule = aws_cloudwatch_event_rule.every_day_at_7_am_gmt.name
   arn  = aws_lambda_function.process_monitoring_queue.arn
 }
